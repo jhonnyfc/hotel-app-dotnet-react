bb70d875ae7381e3a203ca423daf2f3f0072d75a
jhonnyfc <jhonnyfc88@gmail.com>
2024-12-22 17:33:26 +0000

    feat: add new cors poly for preview server

--- hotel-backend/hotel-backend/Program.cs
+++ hotel-backend/hotel-backend/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "http://localhost:4173")
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
