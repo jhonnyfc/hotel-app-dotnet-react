f4d4d49a18fd247fa4e50f196cf3257de2fa39fa
jhonnyfc <jhonnyfc88@gmail.com>
2024-12-22 17:30:51 +0000

    feat: redirect index to hotels

--- hotel-frontend/src/main.tsx
+++ hotel-frontend/src/main.tsx
@@ -1,16 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import HotelsPage from "./hotels/hotelsPage.tsx";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import HotelDetailPage from "./hotelDetail/hotelDetailPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route index element={<Navigate to="hotels" />} />
 
         <Route path="hotels">
           <Route index element={<HotelsPage />} />
